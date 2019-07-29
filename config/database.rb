7054dd679f5793ddfec47bfeb7257f64f983b227
Jeffrey L. Taylor <jeff.taylor@ieee.org>
2019-07-28 19:11:27 -0500

    Add MySQL/MariaDB authentication.

--- config/database.rb
+++ config/database.rb
@@ -1,6 +1,8 @@
 Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
 Sequel::Model.db = case Padrino.env
-  when :development then Sequel.connect("mysql2://localhost/amethyst_development", :loggers => [logger])
-  when :production  then Sequel.connect("mysql2://localhost/amethyst_production",  :loggers => [logger])
-  when :test        then Sequel.connect("mysql2://localhost/amethyst_test",        :loggers => [logger])
+  when :development then Sequel.connect("mysql2://rails:db4ruby@localhost/amethyst_development", :loggers => [logger])
+#  when :production  then Sequel.connect("mysql2://rails:db4ruby@localhost/amethyst_production",  :loggers => [logger])
+  when :production  then Sequel.connect("mysql2://rails:db4ruby@localhost/amethyst_development",  :loggers => [logger])
+  when :test        then Sequel.connect("mysql2://rails:db4ruby@localhost/amethyst_test",        :loggers => [logger])
 end
+Sequel::Model.db.extension(:pagination)
