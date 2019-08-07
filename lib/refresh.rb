b937819ccbe1984c29ac74eb2f344c7d47c883c4
Jeffrey L. Taylor <jeff.taylor@ieee.org>
2019-08-07 17:16:49 -0500

    REFACTOR: Move all Sequel specific code to Feed model.

--- lib/refresh.rb
+++ lib/refresh.rb
@@ -1,6 +1,6 @@
 # All periodic refresh of Feeds policy is in this module.
 #
-require "redis"
+require 'redis'
 
 
 module Refresh
@@ -13,23 +13,27 @@ module Refresh
 
 
   def self.perform
+    # grab time now before lengthy downloads
     now = Time.now
     next_refresh = now + CYCLE_TIME
-    max_refresh = Feed.where{refresh_at <= now + INTERVAL_TIME/2}.count
+    max_refresh = Feed.refreshable(now + INTERVAL_TIME/2).count
 
+    # Refresh distribution of uneven slices
     residue = (@@redis.get(REDIS_KEY) || 0).to_i
     feed_count = Feed.count
-    slice = (feed_count + residue) / INTERVALS
+    slice_size = (feed_count + residue) / INTERVALS
     residue = (feed_count + residue) % INTERVALS
     @@redis.set(REDIS_KEY, residue)
 
-    feeds = Feed.limit(slice).order(:refresh_at).all
+    # Update all Feeds in the slice
+    feeds = Feed.slice(slice_size).all
     feeds.each do |f|
       puts "Refresh: #{f.name} (#{f.refresh_at})."
       f.refresh_at = next_refresh
       f.save
     end
-    
+
+    # Report progress
     tmp = (feeds.size == max_refresh) ? max_refresh : "#{feeds.size}:#{max_refresh}"
     puts "Fetched #{tmp}/#{feed_count} channels at #{Time.now.strftime('%H:%M')}."
   end
--- models/feed.rb
+++ models/feed.rb
@@ -7,4 +7,17 @@ class Feed < Sequel::Model
   def page_number
     1
   end
+
+
+  # Sequel dataset (query) for a slice of the oldest Feeds
+  def self.slice(size)
+    limit(size).order(:refresh_at)
+  end
+
+
+  # Sequel dataset (query) for Feeds to be refreshed on or before limit
+  def self.refreshable(limit)
+#    where{refresh_at <= limit}
+    where(Sequel.lit('refresh_at <= ?', limit))
+  end
 end
