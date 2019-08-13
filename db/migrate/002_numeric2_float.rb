c9b54a7732369b10b4b9584aae41af050a352bc8
Jeffrey L. Taylor <jeff.taylor@ieee.org>
2019-08-13 15:37:44 -0500

    OOPS: score and moving_avg MUST be floating point of some kind.

---
+++ db/migrate/002_numeric2_float.rb
@@ -0,0 +1,15 @@
+Sequel.migration do
+  up do
+    alter_table :feeds do
+      set_column_type :score, :float
+      set_column_type :moving_avg, :float
+    end
+  end
+
+  down do
+    alter_table :feeds do
+      set_column_type :score_avg, Numeric
+      set_column_type :moving_avg, Numeric
+    end
+  end
+end
