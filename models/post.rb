67de4ba5c2d15b2db83b7f246a3ecbba1fe1f02c
Jeffrey L. Taylor <jeff.taylor@ieee.org>
2019-08-15 15:49:06 -0500

    OOPS: substitue String gets HTML escaped; need SafeBuffer.

--- models/post.rb
+++ models/post.rb
@@ -2,6 +2,6 @@ class Post < Sequel::Model
   many_to_one :feed
 
   def name
-    (!title.nil? && !title.empty?) ? title : "<b><em>Post #{id}</em></b>"
+    (!title.nil? && !title.empty?) ? title : SafeBuffer.new("<b><em>Post #{id}</em></b>")
   end
 end
