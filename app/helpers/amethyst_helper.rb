1e5517e091973da24f200a0973b372a4907980f1
Jeffrey L. Taylor <jeff.taylor@ieee.org>
2019-08-19 20:39:58 -0500

    Add flash_class method.

--- app/helpers/amethyst_helper.rb
+++ app/helpers/amethyst_helper.rb
@@ -10,6 +10,17 @@ module Amethyst
         puts "ORIGIN: #{origin}."
         origin
       end
+
+      def flash_class(level)
+        case level
+        when :notice then 'alert alert-info'
+        when :success then 'alert alert-success'
+        when :error then 'alert alert-danger'
+        when :alert then 'alert alert-warning'
+        else
+          'alert-warning'
+        end
+      end
     end
 
     helpers AmethystHelper
