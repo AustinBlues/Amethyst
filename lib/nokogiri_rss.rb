f28ea5582d8ea0acf4be2ca23f372d145f46b202
Jeffrey L. Taylor <jeff.taylor@ieee.org>
2019-09-10 09:56:32 -0500

    Handle missing RSS download.

--- lib/nokogiri_rss.rb
+++ lib/nokogiri_rss.rb
@@ -32,80 +32,81 @@ module NokogiriRSS
       open("|curl -s -4 #{feed.rss_url}") do |rss|
         f = Nokogiri::XML.parse(rss)
 
-        standard = f.at_css('rss').name
-        version = f.at_css('rss')['version']
-
-        unless standard == 'rss' && version == '2.0'
-          feed.status = "#{standard.upcase} #{version}"
-          STDERR.puts("#{feed.status}.")
-        end
-        
-        # Is this what I want, hand-edited title overridden?
-        feed.title ||= f.at_css('channel title').content
-        if feed.title.empty?
-          feed.status = 'missing title'
-          STDERR.puts "MISSING TITLE: '#{feed.name}'."
-        end
-
-        item = f.css('item')
-        if item.size == 0
-          feed.status = 'empty'
-          STDERR.puts "Feed '#{feed.name}' is empty."
+        if f.at_css('rss').nil?
+          STDERR.puts "OOPS(#{feed.name}): #{f.inspect}."
         else
-          item.each do |post|
-            status = nil	# force scope
-#            title = strip_tags(post.at_css('title').content)
-            title = post.at_css('title').content
+          standard = f.at_css('rss').name
+          version = f.at_css('rss')['version']
 
-            if standard == 'rss'
-              description = post.at_css('description').content
-              # NOTE: ident uses .to_s instead of .content for compatibility with RubyRSS module
-              ident = if (tmp = post.at_css('guid'))
-                        tmp.to_s
-                      elsif (tmp = post.at_css('link'))
-                        tmp.to_s
-                      else
-                        status = 'missing indent'
-                        STDERR.puts "NO IDENT: "#{title}'."
-                        title
-                      end
-              date = if (tmp = post.at_css('pubDate'))
-                       tmp.content
-                     elsif (tmp = post.at_css('date'))
-                       status = 'date'
-                       tmp.content
-                     elsif (tmp = post.at_css('dc|date'))
-                       status = 'dc:date'
-                       tmp.content
-                     else
-                       status = 'missing post date'
-                       STDERR.puts "NO DATE: "#{title}'."
-                       now.to_s
-                     end
-              published_at = Time.parse(date)
-            else
-#              description = post.content
-#              ident = post.id.to_s
-#              published_at = strip_tags(post.updated.to_s)
-#              STDERR.puts "ID: #{ident}."
-#              STDERR.puts "UPDATED: #{published_at}."
-              STDERR.puts "METHODS: #{post.methods}"
-            end
+          unless standard == 'rss' && version == '2.0'
+            feed.status = "#{standard.upcase} #{version}"
+            STDERR.puts("#{feed.status}.")
+          end
+          
+          # Is this what I want, hand-edited title overridden?
+          feed.title ||= f.at_css('channel title').content
+          if feed.title.empty?
+            feed.status = 'missing title'
+            STDERR.puts "MISSING TITLE: '#{feed.name}'."
+          end
 
-            Post.update_or_create(feed_id: feed.id, ident: ident) do |p|
-#            Post.find_or_new(feed_id: feed.id, ident: ident) do |p|
-              if p.new?
-                STDERR.puts "NEW: #{title.inspect}."
-                feed.ema_volume += Aging::ALPHA 
-                p.title = title.empty? ? nil : title
-                p.description = description
-                p.published_at = published_at	# TimeDate object
-                p.time = date	# actual String
-                p.url = post.at_css('link').content
+          item = f.css('item')
+          if item.size == 0
+            feed.status = 'empty'
+            STDERR.puts "Feed '#{feed.name}' is empty."
+          else
+            item.each do |post|
+              status = nil	# force scope
+              #            title = strip_tags(post.at_css('title').content)
+              title = post.at_css('title').content
+
+              if standard == 'rss'
+                description = post.at_css('description').content
+                # NOTE: ident uses .to_s instead of .content for compatibility with RubyRSS module
+                ident = if (tmp = post.at_css('guid'))
+                          tmp.to_s
+                        elsif (tmp = post.at_css('link'))
+                          tmp.to_s
+                        else
+                          status = 'missing indent'
+                          STDERR.puts "NO IDENT: "#{title}'."
+                          title
+                        end
+                date = if (tmp = post.at_css('pubDate'))
+                         tmp.content
+                       elsif (tmp = post.at_css('date'))
+                         tmp.content
+                       elsif (tmp = post.at_css('dc|date'))
+                         tmp.content
+                       else
+                         status = 'missing date'
+                         STDERR.puts "NO DATE: "#{title}'."
+                         now.to_s
+                       end
+                published_at = Time.parse(date)
+              else
+                #              description = post.content
+                #              ident = post.id.to_s
+                #              published_at = strip_tags(post.updated.to_s)
+                #              STDERR.puts "ID: #{ident}."
+                #              STDERR.puts "UPDATED: #{published_at}."
+                STDERR.puts "METHODS: #{post.methods}"
               end
-              p.status = status
-              p.previous_refresh = now
-#              STDERR.puts "  #{p.inspect}"
+
+              Post.update_or_create(feed_id: feed.id, ident: ident) do |p|
+                if p.new?
+                  STDERR.puts "NEW: #{title.inspect}."
+                  feed.ema_volume += Aging::ALPHA 
+                  p.title = title.empty? ? nil : title
+                  p.description = description
+                  p.published_at = published_at	# TimeDate object
+                  p.time = date	# actual String
+                  p.url = post.at_css('link').content
+                end
+                p.status = status
+                p.previous_refresh = now
+                #              STDERR.puts "  #{p.inspect}"
+              end
             end
           end
         end
