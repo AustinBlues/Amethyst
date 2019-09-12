4a4ae96fb0a31b4912f43e2c15df19101d6e166d
Jeffrey L. Taylor <jeff.taylor@ieee.org>
2019-09-12 09:01:35 -0500

    Increase EMA to 30 days.

--- lib/aging.rb
+++ lib/aging.rb
@@ -1,8 +1,9 @@
 module Aging
   # Exponential Moving Average (i.e. Feed volume and score) parameters
-  DAYS = 20
+  DAYS = 30
   ALPHA = 2.0/(DAYS + 1.0)
 
+  # done this way so can be used by Resque
   def self.perform
     Feed.age
     Post.zombie_killer
