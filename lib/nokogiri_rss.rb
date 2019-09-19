7e2d548e8ccc8ccc0a98e82f3781dc2734af1a9d
Jeffrey L. Taylor <jeff.taylor@ieee.org>
2019-09-18 19:35:10 -0500

    Add ATOM parsing 1st cut.

--- lib/nokogiri_rss.rb
+++ lib/nokogiri_rss.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require 'nokogiri'
 
 
@@ -12,11 +13,7 @@ module NokogiriRSS
       open("|curl -s -4 #{feed.rss_url}") do |rss|
         f = Nokogiri::XML.parse(rss)
 
-        if f.at_css('rss').nil?
-          feed.status = 'download failed'
-          STDERR.puts "OOPS(#{feed.name}): #{f.inspect}."
-          STDERR.puts "METHODS: #{f.methods}."
-        else
+        if f.at_css('rss')
           standard = f.at_css('rss').name
           version = f.at_css('rss')['version']
 
@@ -33,74 +30,54 @@ module NokogiriRSS
           end
 
           item = f.css('item')
-          if item.size == 0
-            feed.status = 'empty'
-            STDERR.puts "Feed '#{feed.name}' is empty."
-          else
-            item.each do |post|
-              status = nil	# force scope
-              #            title = strip_tags(post.at_css('title').content)
-              title = post.at_css('title').content
+        elsif f.namespaces['xmlns'] =~ /atom/i
+        # ATOM
+          standard = 'ATOM'
+          feed.title = f.at_css('title').content
+          item = f.css('entry')
+        else
+          feed.status = 'download or RSS parse failed'
+#          STDERR.puts "OOPS(#{feed.name}): #{f.inspect}."
+#          STDERR.puts "METHODS: #{f.methods}."
+          item = []
+        end
 
-              if standard == 'rss'
-                description = post.at_css('description').content
-                # NOTE: ident uses .to_s instead of .content for compatibility with RubyRSS module
-                ident = if (tmp = post.at_css('guid'))
-                          tmp.to_s
-                        elsif (tmp = post.at_css('link'))
-                          tmp.to_s
-                        else
-                          status = 'missing indent'
-                          STDERR.puts "NO IDENT: "#{title}'."
-                          title
-                        end
-                date = if (tmp = post.at_css('pubDate'))
-                         tmp.content
-                       elsif (tmp = post.at_css('date'))
-                         tmp.content
-                       elsif (tmp = post.at_css('dc|date'))
-                         tmp.content
-                       else
-                         status = 'missing date'
-                         STDERR.puts "NO DATE: "#{title}'."
-                         now.to_s
-                       end
-                published_at = Time.parse(date)
-              else
-                #              description = post.content
-                #              ident = post.id.to_s
-                #              published_at = strip_tags(post.updated.to_s)
-                #              STDERR.puts "ID: #{ident}."
-                #              STDERR.puts "UPDATED: #{published_at}."
-                STDERR.puts "METHODS: #{post.methods}"
-              end
+        if item.size == 0
+          feed.status = 'empty'
+          STDERR.puts "Feed '#{feed.name}' is empty."
+        else
+          item.each do |post|
+            
+            attrs = case standard
+                    when 'rss'
+                      parse_rss_item(post)
+                    when 'ATOM'
+                      parse_atom_item(post)
+                    else
+                      #              description = post.content
+                      #              ident = post.id.to_s
+                      #              published_at = strip_tags(post.updated.to_s)
+                      #              STDERR.puts "ID: #{ident}."
+                      #              STDERR.puts "UPDATED: #{published_at}."
+                      STDERR.puts "METHODS: #{post.methods}"
+                      nil
+                    end
 
-              Post.update_or_create(feed_id: feed.id, ident: ident) do |p|
-                if p.new?
-                  STDERR.puts "NEW: #{title.inspect}."
-                  feed.ema_volume += Aging::ALPHA 
-                  p.title = title.empty? ? nil : title
-                  p.description = description
-                  p.published_at = published_at	# TimeDate object
-                  p.time = date	# actual String
-                  p.url = if !(link = post.at_css('link')).nil?
-                            link.content
-                          elsif !(link = post.at_css('enclosure')).nil?
-                            link['url']
-                          else
-                            p.status = 'missing URL'
-                            STDERR.puts "MISSING URL: '#{p.name}'."
-                            feed.rss_url
-                          end
-                end
-                p.status = status
-                p.previous_refresh = now
-                #              STDERR.puts "  #{p.inspect}"
+            Post.update_or_create(feed_id: feed.id, ident: attrs[:ident]) do |p|
+#              puts "ATTRS: #{attrs.inspect}."
+              if p.new?
+                STDERR.puts "NEW: #{attrs[:title]}."
+                feed.ema_volume += Aging::ALPHA 
+                
+                p.set(attrs)
               end
+              p.previous_refresh = now
+              #              STDERR.puts "  #{p.inspect}"
             end
           end
         end
       end
+      
     rescue Exception => e
       STDERR.puts e.backtrace.join('\n')
       feed.status = e.class
@@ -117,5 +94,88 @@ module NokogiriRSS
     else
       puts "Refreshed (no previous refresh): #{feed.name}."
     end
+  end
+
+
+  def parse_rss_item(post)
+    attrs = {}
+
+    attrs[:title] = post.at_css('title').content
+    attrs[:description] = post.at_css('description').content
+    # NOTE: ident uses .to_s instead of .content for compatibility with RubyRSS module
+    attrs[:ident] = if (tmp = post.at_css('guid'))
+                      tmp.to_s
+                    elsif (tmp = post.at_css('link'))
+                      tmp.to_s
+                    else
+                      attrs[:status] = 'missing ident'
+                      STDERR.puts "NO IDENT: "#{title}'."
+                      post.feed.title
+                    end
+    attrs[:time] = if (tmp = post.at_css('pubDate'))
+                     tmp.content
+                   elsif (tmp = post.at_css('date'))
+                     tmp.content
+                   elsif (tmp = post.at_css('dc|date'))
+                     tmp.content
+                   else
+                     attrs[:status] = 'missing date'
+                     STDERR.puts "NO DATE: "#{title}'."
+                     now.to_s
+                   end
+    attrs[:published_at] = Time.parse(attrs[:time])
+    attrs[:url] = if !(link = post.at_css('link')).nil?
+                   link.content
+                 elsif !(link = post.at_css('enclosure')).nil?
+                   link['url']
+                 else
+                   p.status = 'missing URL'
+                   STDERR.puts "MISSING URL: '#{p.name}'."
+                   post.feed.rss_url
+                 end
+    attrs
+  end
+
+
+  def parse_atom_item(post)
+    attrs = {}
+
+    attrs[:title] = post.at_css('title').content
+    attrs[:description] = post.at_css('content').content
+    if (tmp = post.at_css('summary'))
+      attrs[:synopsis] = tmp.content
+    end
+    # NOTE: ident uses .to_s instead of .content for compatibility with RubyRSS module
+    attrs[:ident] = if (tmp = post.at_css('id'))
+                      tmp.to_s
+                    elsif (tmp = post.at_css('link'))
+                      tmp['href']
+                    else
+                      attrs[:status] = 'missing ident'
+                      STDERR.puts "NO IDENT: "#{title}'."
+                      post.feed.title
+                    end
+    attrs[:time] = if (tmp = post.at_css('published'))
+                     tmp.content
+                   elsif (tmp = post.at_css('date'))
+                     tmp.content
+                   elsif (tmp = post.at_css('dc|date'))
+                     tmp.content
+                   else
+                     attrs[:status] = 'missing date'
+                     STDERR.puts "NO DATE: "#{title}'."
+                     now.to_s
+                   end
+    attrs[:published_at] = Time.parse(attrs[:time])
+    attrs[:url] = if !(link = post.at_css('link')).nil?
+                   link['href']
+                 elsif !(link = post.at_css('enclosure')).nil?
+                   link['url']
+                 else
+                   p.status = 'missing URL'
+                   STDERR.puts "MISSING URL: '#{p.name}'."
+                   post.feed.rss_url
+                 end
+    attrs
   end
 end
