5c7788fdf00f46b5636e9d58055cc67c1593fb9b
Jeffrey L. Taylor <jeff.taylor@ieee.org>
2019-12-13 09:18:59 -0600

    1st cut at tooltip for Feeds in index.

--- app/helpers/feed_helper.rb
+++ app/helpers/feed_helper.rb
@@ -6,6 +6,24 @@ module Amethyst
       # def simple_helper_method
       # ...
       # end
+
+      # format a score
+      def score_fmt(score, fmt = '%0.2f')
+        if score.nil?
+          'NULL' 
+        else
+          fmt % score
+        end
+      end
+
+      # format an average
+      def avg_fmt(avg)
+        '%0.2f' % avg
+      end
+
+      def tooltip_fmt(feed)
+        "Score: #{score_fmt(feed.score)} Click: #{feed.clicks} Hide: #{feed.hides} Down: #{feed.down_votes} (Vol: #{avg_fmt(feed.ema_volume)})"
+      end
     end
 
     helpers FeedHelper
