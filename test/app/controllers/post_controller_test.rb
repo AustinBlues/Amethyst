b6884bdd64da51d3616327010c29d1ca0a1d8f75
Jeffrey L. Taylor <jeff.taylor@ieee.org>
2020-07-20 18:14:24 -0500

    Clean up code.

--- test/app/controllers/post_controller_test.rb
+++ test/app/controllers/post_controller_test.rb
@@ -1,16 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../test_config.rb')
-require 'ruby_rss'
-
 
 describe "/post" do
-  include RubyRSS
-  
-#  before do
-#    get "/post"
-#  end
+  before do
+    get "/post"
+  end
 
   it "should return Posts index" do
-    get "/post"
     assert_match(/Posts/, last_response.body)
   end
 end
