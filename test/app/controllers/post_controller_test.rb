94de922fc1ce501471f4d30777806a53f8e2ddab
Jeffrey L. Taylor <jeff.taylor@ieee.org>
2020-07-20 12:59:17 -0500

    first_nonblank() has nothing to do with Post controller.

--- test/app/controllers/post_controller_test.rb
+++ test/app/controllers/post_controller_test.rb
@@ -13,26 +13,4 @@ describe "/post" do
     get "/post"
     assert_match(/Posts/, last_response.body)
   end
-
-  it 'first_nonblank should return nil for nil or blank args' do
-    [nil, '', ' '].each do |arg|
-      assert_nil first_nonblank(arg)
-    end
-  end
-
-  it 'first_nonblank should return arg for blank arg' do
-    arg = Time.now.to_s
-    assert_equal arg, first_nonblank(arg)
-  end
-
-  it 'first_nonblank should return nil for nil or blank second args' do
-    [nil, '', ' '].each do |arg|
-      assert_nil first_nonblank(nil, arg)
-    end
-  end
-
-  it 'first_nonblank should return arg for blank second arg' do
-    arg = Time.now.to_s
-    assert_equal arg, first_nonblank(nil, arg)
-  end
 end
