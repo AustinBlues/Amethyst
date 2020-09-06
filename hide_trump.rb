116212761e145299a55d291a8d4fdf57c4cb6897
Jeffrey L. Taylor <jeff.taylor@ieee.org>
2020-09-06 16:53:51 -0500

    Move sludge filter into main code.

--- hide_trump.rb
+++
@@ -1,3 +0,0 @@
-Sludge.filter(nil, 'Trump Mnuching Kudlow China')
-#Sludge.filter(Feed.where(title: 'Al Jazeera English').first, 'Trump Mnuching Kudlow China')
-#Sludge.filter(Feed.where(title: 'The Hill - The Hill News').first, 'Trump Mnuching Kudlow China')
