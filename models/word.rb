6a8afdb9f6121f1fb530a98912139ce460a4d735
Jeffrey L. Taylor <jeff.taylor@ieee.org>
2021-01-09 18:36:30 -0600

    Add aging for Words with twice the period of score aging.

--- models/word.rb
+++ models/word.rb
@@ -1,4 +1,8 @@
 class Word < Sequel::Model
   many_to_many :word, join_table: :contexts, left_key: :prev_id, right_key: :next_id
   many_to_many :post, join_table: :occurrences
+
+  def self.age
+    dataset.update(frequency: Sequel[:frequency]*(1.0 - Aging::ALPHA2))
+  end
 end
