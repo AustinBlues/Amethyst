a722add67b7df0429c05fc63859330bdf49ed6c0
Jeffrey L. Taylor <jeff.taylor@ieee.org>
2021-01-28 10:25:43 -0600

    OOPS: correct down_votes, still needed.

--- db/migrate/010_drop_unused_columns.rb
+++ db/migrate/010_drop_unused_columns.rb
@@ -3,7 +3,6 @@ Sequel.migration do
     alter_table :feeds do
       drop_column :debug
       drop_column :encoding
-      drop_column :down_votes
     end
     alter_table :posts do
       drop_column :undo_queue
@@ -16,7 +15,6 @@ Sequel.migration do
     alter_table :feeds do
       add_column :debug, String
       add_column :encoding, Integer, limit: 1
-      add_column :down_votes, Integer
     end
     alter_table :posts do
       add_column :undo_queue, Integer
