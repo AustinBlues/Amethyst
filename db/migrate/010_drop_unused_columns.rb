25ea435eec8b7ae1bae70d8ebe82272071bf9120
Jeffrey L. Taylor <jeff.taylor@ieee.org>
2021-01-27 20:11:40 -0600

    OOPS: downvotes in wrong table.

--- db/migrate/010_drop_unused_columns.rb
+++ db/migrate/010_drop_unused_columns.rb
@@ -3,11 +3,11 @@ Sequel.migration do
     alter_table :feeds do
       drop_column :debug
       drop_column :encoding
+      drop_column :down_votes
     end
     alter_table :posts do
       drop_column :undo_queue
       drop_column :readability
-      drop_column :down_votes
       drop_column :status
     end
   end
@@ -16,11 +16,11 @@ Sequel.migration do
     alter_table :feeds do
       add_column :debug, String
       add_column :encoding, Integer, limit: 1
+      add_column :down_votes, Integer
     end
     alter_table :posts do
       add_column :undo_queue, Integer
       add_column :readability, TrueClass, default: false, null: false
-      add_column :down_votes, Integer
       add_column :status, String
     end
   end
