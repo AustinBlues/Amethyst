f5f6d909a76a05d82cc84a051f7b9e521e375fb7
Jeffrey L. Taylor <jeff.taylor@ieee.org>
2021-01-29 10:59:53 -0600

    Delete debugging printout.

--- lib/resque_init.rb
+++ lib/resque_init.rb
@@ -2,4 +2,3 @@ padrino_env   = ENV["PADRINO_ENV"] ||= ENV["RACK_ENV"] ||= "development"
 resque_config = YAML.load_file Padrino.root('config','resque.yml')
 Resque.redis  = resque_config[padrino_env]
 Resque.redis.namespace = ROOT
-puts "In #{__FILE__}."
