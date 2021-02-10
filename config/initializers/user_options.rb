e670f30bcf811682667ecc8970ed26b5ff70aaaa
Jeffrey L. Taylor <jeff.taylor@ieee.org>
2021-02-09 21:38:10 -0600

    Move EMA period to user_options.

--- config/initializers/user_options.rb
+++ config/initializers/user_options.rb
@@ -8,9 +8,13 @@ DAYS_OF_THE_DEAD = 34
 # How many Unread post to keep visible
 UNREAD_LIMIT = 50
 
+# How many days for Feed score Exponential Moving Average (EMA)
+# How long between posts of least frequent post is a good starting value
+EMA_DAYS = 30
+
 
 PAGINATION = ENV['AMETHYST_PAGINATION'] ? ENV['AMETHYST_PAGINATION'].to_i(2) : TOP_PAGINATION
-PAGINATED = PAGINATION != 0
+PAGINATED = (PAGINATION != 0)
 ROOT = ENV['ROOT'] || ENV['PWD'].split('/').last
 
 DISPLAY_WORDS = true
--- lib/daily.rb
+++ lib/daily.rb
@@ -1,10 +1,12 @@
 module Daily
+  extend Amethyst::App::PostHelper
+
   # Exponential Moving Average (i.e. Feed volume and score) parameters
-  DAYS = 90
-  ALPHA = 2.0/(DAYS + 1.0)
+  ALPHA = 2.0/(EMA_DAYS + 1.0)
 
   # done this way so can be used by Resque
   def self.perform
+    Refresh.log "Daily maintenance at #{short_datetime(Time.now)}."
     Feed.age
     Post.zombie_killer
   end
