55d5b609c966a58589af4533ca1bc444687296fc
Jeffrey Taylor <jeff.taylor@ieee.org>
2021-05-05 20:18:50 -0500

    Double EMA.

--- config/initializers/user_options.rb
+++ config/initializers/user_options.rb
@@ -9,8 +9,8 @@ DAYS_OF_THE_DEAD = 34
 UNREAD_LIMIT = 50
 
 # How many days for Feed score Exponential Moving Average (EMA)
-# How long between posts of least frequent post is a good starting value
-EMA_DAYS = 30
+# Twice as long between posts of least frequent post is a good starting value
+EMA_DAYS = 60
 
 PAGINATION = ENV['AMETHYST_PAGINATION'] && (Padrino.env != :test) ? ENV['AMETHYST_PAGINATION'].to_i(2) : TOP_PAGINATION
 PAGINATED = (PAGINATION != 0)
