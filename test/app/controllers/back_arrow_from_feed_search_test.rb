56590838743e86961edcbee29b49ffc35081bac9
Jeffrey Taylor <jeff.taylor@ieee.org>
2021-05-08 12:06:47 -0500

    OOPS: missed this in refresh convention change.

--- test/app/controllers/back_arrow_from_feed_search_test.rb
+++ test/app/controllers/back_arrow_from_feed_search_test.rb
@@ -6,7 +6,7 @@ describe "/feed" do
     Feed.all{|f| f.destroy}
     # Create Feed and Posts in database
     now = Time.now - PAGE_SIZE
-    @feed = Feed.create(title: 'Feed 1', rss_url: 'http://127.0.0.1', previous_refresh: now)
+    @feed = Feed.create(title: 'Feed 1', rss_url: 'http://127.0.0.1', next_refresh: now)
     @posts = (PAGE_SIZE+5).times.map do |i|
       Post.create(title: "Post #{i+1}", feed_id: @feed[:id], ident: i, url: "http://127.0.0.1/#{i}",
                   description: "Post #{i+1} content.", published_at: now+i)
