b3219ef78bd79ea3ec73016d7cb0ff0dcc966b20
Jeffrey Taylor <jeff.taylor@ieee.org>
2021-06-07 12:53:00 -0500

    Delete debugging statements.

--- lib/nokogiri_rss.rb
+++ lib/nokogiri_rss.rb
@@ -13,10 +13,7 @@ module NokogiriRSS
 #      open("|curl -s -4 '#{feed.rss_url}'") do |rss|
       rss = %x(wget '#{feed.rss_url}' -4 -q -O -)
       if $?.to_s !~ /exit 0/
-#        puts("WGET: #{$?}.")
         rss = %x(curl -s -4 '#{feed.rss_url}')
-#        puts "RSS: #{rss.size} bytes."
-#        puts "RSS: #{rss}."
       end
 
       f = Nokogiri::XML.parse(rss)
