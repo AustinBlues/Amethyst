86eb7174d27e501a1b1d101722fba82ee913330d
Jeffrey Taylor <jeff.taylor@ieee.org>
2021-09-23 11:57:05 -0500

    Check for valid URL immediately.  Fail back to origin.

--- app/controllers/feed.rb
+++ app/controllers/feed.rb
@@ -58,35 +58,40 @@ Amethyst::App.controllers :feed do
   
 
   post :create, '/feed' do
-    # I'm surprised I have to do this.
-    params.delete('authenticity_token')
+    params[:rss_url].strip!
+    if !URI.parse(params[:rss_url]).kind_of?(URI::HTTP)
+      flash[:error] = "'#{params[:rss_url]}' is not valid."
+      redirect @origin
+    else
+      params.delete('authenticity_token')      # I'm surprised I have to do this.
 
-    begin
       params[:next_refresh] = Time.now
-      w = Feed.create(params)
-    rescue Sequel::UniqueConstraintViolation => e
-      if /unique_(\w+)s'/ !~ e.to_s
-        STDERR.puts "'#{params[:rss_url]}' already followed."
-        w = Feed.where(rss_url: params[:rss_url]).first
-        flash[:warning] = "'#{w.name}' already followed."
-      else
-        flash[:error] = "Duplicate #{$~[1]}."
-        case $~[0]
-        when 'title'
-          w = Feed.where(title: params[:title]).first
-        when 'filename'
-          w = Feed.where(filename: params[:filename]).first
-        else
-          w = Feed.first
-        end
-      end
-    rescue Exception => e
-      flash[:error] = "Unknown exception: #{e}."
-      w = Feed.first
-    end
 
-    # Redirect to index where new feed will appear.
-    redirect url_for(:feed, :index, page: w.page_number)
+      begin
+        w = Feed.create(params)
+      rescue Sequel::UniqueConstraintViolation => e
+        if /unique_(\w+)s'/ !~ e.to_s
+          STDERR.puts "'#{params[:rss_url]}' already followed."
+          w = Feed.where(rss_url: params[:rss_url]).first
+          flash[:warning] = "'#{w.name}' already followed."
+        else
+          flash[:error] = "Duplicate #{$~[1]}."
+          case $~[0]
+          when 'title'
+            w = Feed.where(title: params[:title]).first
+          when 'filename'
+            w = Feed.where(filename: params[:filename]).first
+          else
+            w = Feed.first
+          end
+        end
+      rescue Exception => e
+        flash[:error] = "Unknown exception: #{e}."
+      end
+
+      # Redirect to index where new feed will appear.
+      redirect url_for(:feed, :index, page: w.page_number)
+    end
   end
 
 
@@ -121,9 +126,8 @@ Amethyst::App.controllers :feed do
     if feed
       feed[:next_refresh] = nil
       feed.save(changed: true)
-      # Initial queue is higher priority than Refresh queue
       Resque.enqueue_to('Initial', Refresh, -feed[:id])
-      flash[:success] = 'Delete queued.'
+      flash[:success] = 'Delete queued'
 
       redirect url(:feed, :index)
     else
