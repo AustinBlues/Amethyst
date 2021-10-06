de7b9df5dce0c6ce86d14c20bf0029b37500ae42
Jeffrey Taylor <jeff.taylor@ieee.org>
2021-10-06 18:19:07 -0500

    Implement new understanding of how Minitest spec works.

--- test/app/controllers/post_controller_test.rb
+++ test/app/controllers/post_controller_test.rb
@@ -12,6 +12,7 @@ describe "/post" do
       Post.create(title: "Post #{i+1}", feed_id: @feed[:id], ident: i, url: "http://127.0.0.1/#{i}",
                   description: "Post #{i+1} content.", published_at: now+i)
     end
+    @origin = '/post?page=2'
   end
 
   after do
@@ -20,15 +21,43 @@ describe "/post" do
 
 
   describe 'when displaying Posts index' do
-    it 'should return Post index, page 2' do
-      get '/post?page=2'
+    it 'should identify as Post index with correct first Post and links' do
+      get @origin
+
       assert_match(/Posts/, last_response.body)
       assert_match(/#{@feed[:title]}/, last_response.body)
 
       p = Nokogiri::HTML.parse(last_response.body)
+      
+      link = p.at_css('.card-header a.btn')
+      assert_equal(link.attr('title'), 'to Feeds')
+      assert_equal(link.attr('href'), '/feed')
 
-      # check Post show has expected title
-      assert_equal(@posts[EXTRA-1][:title], p.at_css('td a').content.strip)
+      # check first Post has expected title
+      assert_equal(@posts[EXTRA-1][:title], p.at_css('td:first-child a').content.strip)
+
+      # check first Post has correct action links
+      links = p.css('tbody:first-child a.btn')
+
+      # HIDE, and DOWN links
+      assert_equal("/post/#{@posts[EXTRA-1][:id]}/hide?page=2", links[0].attr('href'))
+      assert_equal("/post/#{@posts[EXTRA-1][:id]}/down?page=2", links[1].attr('href'))
+    end
+  end
+
+  describe 'when displaying Post show' do
+    if false
+      # check HIDE and DOWN have expected redirect
+      hide = links[2].attr('href')
+      assert_equal("/post/#{@posts[0][:id]}/hide?origin=#{CGI.escape(@origin)}", hide)
+      get hide
+      puts "RESPONSE: #{last_response.body}"
+      assert_equal("http://example.org#{@origin}", last_response.location.encode('utf-8'))
+
+      down = links[3].attr('href')
+      assert_equal("/post/#{@posts[0][:id]}/down?origin=#{CGI.escape(@origin)}", down)
+      get down
+      assert_equal("http://example.org#{@origin}", last_response.location.encode('utf-8'))
     end
   end
 end
