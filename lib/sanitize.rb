f08a998c61409001de4cd3073d25fddf7d6bb171
Jeffrey Taylor <jeff.taylor@ieee.org>
2021-10-18 21:10:55 -0500

    Creep improved encoding back in.

--- lib/sanitize.rb
+++ lib/sanitize.rb
@@ -5,20 +5,18 @@ module Sanitize
 
   def sanitize(which, max_length)
     if self[which].nil?
-      # preserve nil
       valid = true
     else
       original_length = self[which].length
       if self.respond_to?('feed') && self.feed.title == 'AustinGO RSS'
+        # Workarounds for AustinGO RSS feed mangled encoding.  Delete when fixed.
         self[which].gsub!(/â€™/, "\u{2019}")	# single right quote
-        self[which].gsub!(/Â/, "\u{A0}")	# non-breaking space?
-        self[which].gsub!(/👉/, "\u{261e}")	# right pointing hand, and maybe a skin tone character
-        self[which].gsub!('&#039;', %q{'})
+        self[which].gsub!(/Â/, "\u{A0}")	# non-breaking space
+        self[which].gsub!(/👉/, "\u{261e}")	# white right pointing index
         self[which].encode!('UTF-8', invalid: :replace, undef: :replace)
       end
       valid = (original_length == self[which].length)
-      # line below corrupts something and MariaDB barfs on 'Incorrect string value'
-#      self[which] = @@entities_decoder.decode(self[which])
+      self[which] = @@entities_decoder.decode(self[which])
     end
     valid
   end
