fd2bb27951ecf53789c328c159f9141a8ee654ba
Jeffrey Taylor <jeff.taylor@ieee.org>
2021-10-21 12:43:49 -0500

    Change method name from sanitize to sanitize! to reflect that is changes values. Flip sense of return value, true if makes changes. Improve how changes are detected.

--- lib/sanitize.rb
+++ lib/sanitize.rb
@@ -8,16 +8,22 @@ module Sanitize
       sanitized = false
     else
       original_length = self[which].length
+      sanitized = false
       if self.respond_to?('feed') && self.feed.title == 'AustinGO RSS'
         # Workarounds for AustinGO RSS feed mangled encoding.  Delete when fixed.
         self[which].gsub!(/â€™/, "\u{2019}")	# single right quote
+        sanitized ||= !$~.nil?
         self[which].gsub!(/Â/, "\u{A0}")	# non-breaking space
+        sanitized ||= !$~.nil?
 #        self[which].gsub!(/👉/, "\u{1F449}")	# white right pointing index (Incorrect string value on MariaDB)
         self[which].gsub!(/👉/, "\u{261E}")	# right pointing index
+        sanitized ||= !$~.nil?
         self[which].encode!('UTF-8', invalid: :replace, undef: :replace)
+        sanitized ||= !$~.nil?
       end
       self[which].gsub!("\u{1F449}", "\u{261E}")	# MariaDB doesn't accept Unicode white right pointing index
-      sanitized = (original_length != self[which].length)
+      sanitized ||= !$~.nil?
+#      sanitized = (original_length != self[which].length)
       self[which] = @@entities_decoder.decode(self[which])
     end
     sanitized
