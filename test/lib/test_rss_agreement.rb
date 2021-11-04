79a16df4eeaef90038513f64a8280e7e2863eccd
Jeffrey Taylor <jeff.taylor@ieee.org>
2021-11-04 18:55:25 -0500

    Expand to test multiple files.

--- test/lib/test_rss_agreement.rb
+++ test/lib/test_rss_agreement.rb
@@ -11,36 +11,58 @@ module Ruby
   extend RubyRSS
 end
 
+FILES = %w{spectrum1.rss dw.rdf csps.atom}
 
 describe 'NokogiriRSS and RubyRSS agreement' do
   before do
-    @nokogiri_url = "#{File.expand_path(File.dirname(__FILE__))}/../../test_data/spectrum1.rss"
-    @nokogiri_feed = Feed.create(rss_url: @nokogiri_url, title: 'NokogiriRSS')
-    ruby_url = "../../test_data/spectrum1.xml"
-    @ruby_feed = Feed.create(rss_url: ruby_url, title: 'RubyRSS')
+    Feed.all{|f| f.destroy}
   end
 
   after do
-    @nokogiri_feed.destroy if @nokogiri_feed
-    @ruby_feed.destroy if @ruby_feed
+    Feed.all{|f| f.destroy}
   end
 
   it 'should match regardless of which RSS parser used' do
-    File.open(@nokogiri_url) do |f|
-      rss = f.read
-      now = Time.now
+    FILES.each do |name|
+      nokogiri_url = "#{File.expand_path(File.dirname(__FILE__))}/../../test_data/#{name}"
+      nokogiri_feed = Feed.create(rss_url: nokogiri_url, title: 'NokogiriRSS')
+      ruby_url = "../../test_data/#{name}"
+      ruby_feed = Feed.create(rss_url: ruby_url, title: 'RubyRSS')
 
-      Nokogiri.refresh_feed(@nokogiri_feed, rss, now)
-      @nokogiri_feed.reload
+      File.open(nokogiri_url) do |f|
+        rss = f.read
+        now = Time.now
+        if name =~ /\.(\w+)/
+          kind = $~[1]
+          kind.upcase!
+        else
+          STDERR.puts "Missing file extension"
+          exit
+        end
 
-      Ruby.refresh_feed(@ruby_feed, rss, now)
-      @ruby_feed.reload
+        Nokogiri.refresh_feed(nokogiri_feed, rss, now)
+        nokogiri_feed.reload
 
-      @nokogiri_feed.post.each_with_index do |post, i|
-        [:ident, :title, :description, :url].each do |tag|
-          assert_equal post[tag], @ruby_feed.post[i][tag]
+        Ruby.refresh_feed(ruby_feed, rss, now)
+        ruby_feed.reload
+
+        max = if nokogiri_feed.post.size == ruby_feed.post.size
+                nokogiri_feed.post.size - 1
+              else
+                STDERR.puts "Post count mismatch: #{nokogiri_feed.post.size} vs. #{ruby_feed.post.size}."
+                max = [nokogiri_feed.post.size, ruby_feed.post.size].min - 1
+              end
+          
+        0.upto(max).each do |i|
+          [:title, :url, :ident].each do |tag|
+            assert_equal nokogiri_feed.post[i][tag], ruby_feed.post[i][tag], "#{kind} #{tag.to_s} mismatch"
+          end
+          tag = :description
+          assert_equal  nokogiri_feed.post[i][tag][0..79], ruby_feed.post[i][tag][0..79], "#{kind} description mismatch"
         end
       end
+      nokogiri_feed.destroy
+      ruby_feed.destroy
     end
   end
 end
