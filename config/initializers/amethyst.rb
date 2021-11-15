ecfdce298e73f404c736536b242c0e3094fc774c
Jeffrey Taylor <jeff.taylor@ieee.org>
2021-11-15 17:09:10 -0600

    Tweak navigation and action links appearence.

--- config/initializers/amethyst.rb
+++ config/initializers/amethyst.rb
@@ -14,7 +14,8 @@ BOTTOM_PAGINATION = FEED_BOTTOM | POST_BOTTOM
 
 # HTML entities & Icons
 ADD_ICON = SafeBuffer.new('&plus;')
-BACK_ICON = SafeBuffer.new('&larr;')
+#BACK_ICON = SafeBuffer.new('&larr;')
+BACK_ICON = "\u{2190}"
 DELETE_ICON = SafeBuffer.new('&minus;')
 DOWN_VOTE_ICON = SafeBuffer.new('&darr;')
 EDIT_ICON = "\u270E"
@@ -27,12 +28,13 @@ SEARCH_ICON = "\u{1F50E}"
 #SWAP_ICON = '⇅'
 #SWAP_ICON = '↕'
 #SWAP_ICON = "\u{1F5D8}"
-SWAP_ICON = SafeBuffer.new('<img src="cycle.png" alt="Cycle thru indexes" style="width:32px;height:32px;">')
+#SWAP_ICON = SafeBuffer.new('<img src="cycle.png" alt="Cycle thru indexes" style="width:32px;height:32px;">')
+SWAP_ICON = "\u{21c4}"
 TIMES = SafeBuffer.new('&times;')
 #TO_FEED_ICON = "\u{25B2}"	# up pointing triangle
-TO_FEED_ICON = SWAP_ICON
+TO_FEED_ICON = "\u{21c6}"
 #TO_POST_ICON = "\u{25BC}"	# down pointing triangle
-TO_POST_ICON = SWAP_ICON
+TO_POST_ICON = "\u{21c4}"
 #UNDO_ICON = "\u{21A9}"	# left hook arrow
 UNDO_ICON = '↩'
 
