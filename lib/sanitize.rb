336c0860263005e12ffd159f73e96a3a4602b0bd
Jeffrey Taylor <jeff.taylor@ieee.org>
2021-11-17 18:06:30 -0600

    Delete code specific to AustinGO RSS.

--- lib/sanitize.rb
+++ lib/sanitize.rb
@@ -4,22 +4,8 @@ module Sanitize
   @@entities_decoder = HTMLEntities.new	# DB handles UTF-8/Unicode
 
   def sanitize!(which, max_length)
-    if self[which].nil?
-      sanitized = false
-    else
-      sanitized = false
-      if self.respond_to?('feed') && self.feed[:title] == 'AustinGO RSS'
-        # Workarounds for AustinGO RSS feed mangled encoding.  Delete when fixed.
-        self[which].gsub!(/â€™/, "\u{2019}")	# single right quote
-        sanitized ||= !$~.nil?
-        self[which].gsub!(/Â/, "\u{A0}")	# non-breaking space
-        sanitized ||= !$~.nil?
-#        self[which].gsub!(/👉/, "\u{1F449}")	# white right pointing index (Incorrect string value on MariaDB)
-        self[which].gsub!(/👉/, "\u{261E}")	# right pointing index
-        sanitized ||= !$~.nil?
-        self[which].encode!('UTF-8', invalid: :replace, undef: :replace)
-        sanitized ||= !$~.nil?
-      end
+    sanitized = false
+    if self[which]
       self[which].gsub!("\u{1F449}", "\u{261E}")	# MariaDB doesn't accept Unicode white right pointing index
       sanitized ||= !$~.nil?
       original_length = self[which].length
