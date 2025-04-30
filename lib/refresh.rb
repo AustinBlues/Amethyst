4fd3d4d9435906b2246bbcde0e1ff8585e398f52
Jeffrey Taylor <jeff.taylor@ieee.org>
2025-04-30 17:42:28 -0500

    Revert "If no refresh for an hour, refresh all."
    This reverts commit 10830c0c273cdf00ab9e5e9870f735b98d3da507.

--- lib/refresh.rb
+++ lib/refresh.rb
@@ -221,13 +221,8 @@ module Refresh
     if slice_size == 0
       log "Nothing to fetch at #{Time.now.strftime('%l:%M%P').strip}."
     else
-      feeds = if max_refresh <= slice_size
-                # Update all Feeds in the slice
-                Feed.slice(slice_size, now + INTERVAL_TIME/2)
-              else
-                # Update all Feeds
-                Feed.slice(max_refresh, now + CYCLE_TIME)
-              end
+      # Update all Feeds in the slice
+      feeds = Feed.slice(slice_size, now + INTERVAL_TIME/2)
       if (fetch_cnt = feeds.count) <= 0
         log "Too early to fetch feeds at #{Time.now.strftime('%l:%M%P').strip}.", :info
       else
